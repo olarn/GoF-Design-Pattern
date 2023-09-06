89d46a2194004a5d8ee16de13c53a7c281680800
Olarn U <olarn.u@odds.team>
2023-09-06 11:48:00 +0700

    Add begin code for strategy pattern.

---
+++ code/src/strategy/begin/billing.test.ts
@@ -0,0 +1,52 @@
+import { Billing, PackageType } from "./billing";
+
+describe('Generate monthly billing based-on total hours and package type', () => {
+    it('should always return package price for fixed package', () => {
+        // given
+        const totalHours = 10;
+        const packageType = PackageType.FIXED;
+
+        // when
+        const billing = new Billing(totalHours, packageType);   
+
+        // then
+        expect(billing.monthlyBill()).toBe(535);
+    });
+
+    it('should always return total hours * 50 for variable package', () => {
+        // given
+        const totalHours = 10;
+        const packageType = PackageType.HOUR_FLEX;
+
+        // when
+        const billing = new Billing(totalHours, packageType);
+
+        // then
+        expect(billing.monthlyBill()).toBe(535);
+    });
+
+    it('should always return 0 for unknown package', () => {
+        // given
+        const totalHours = 10;
+        const packageType = 'UNKNOWN';
+
+        // when
+        const billing = new Billing(totalHours, packageType);
+
+        // then
+        expect(billing.monthlyBill()).toBe(0); 
+    });
+
+    it('should return 75.0 for stepping package', () => { 
+        // given
+        const totalHours = 100.0;
+        // Stepping Package is 1THB for first 50 Hr, 0.5THB for the rest 
+        const packageType = PackageType.STEPPING;
+
+        // when
+        const billing = new Billing(totalHours, packageType);
+
+        // then
+        expect(billing.monthlyBill()).toBe(75.0);
+    });
+}); 
---
+++ code/src/strategy/begin/billing.ts
@@ -0,0 +1,29 @@
+export enum PackageType {
+    FIXED = 'FIXED',
+    HOUR_FLEX = "HOUR_FLEX",
+    STEPPING = "STEPPING"
+}
+
+export class Billing {
+    private vatRate = 7.0;
+    private totalHours: number;
+    private packageType: string;
+
+    constructor(totalHours: number, packageType: string) {
+        this.totalHours = totalHours;
+        this.packageType = packageType;
+    }
+
+    public monthlyBill(): number {
+        var total = 0.0;
+        if (this.packageType === PackageType.FIXED) {
+            total = 500;
+        } else 
+        if (this.packageType === PackageType.HOUR_FLEX) {
+            total = this.totalHours * 50;
+        } else 
+            total = 0;
+        
+        return total + (total * this.vatRate / 100);
+    }
+}
