6a6de89e26a3131b70622da7a367d2fa29f3f4bd
Olarn U <olarn.u@odds.team>
2023-09-26 15:42:37 +0700

    Add Adaptor example code files to finish and lab folder.

---
+++ code/src/adaptor/lab/mediaPlayer.test.ts
@@ -0,0 +1,21 @@
+import { MediaPlayer } from "./mediaPlayer";
+import { SpotifyAPI } from "./spotifyAPI";
+
+describe('Media Player', () => {
+
+    it('should get nothing if not specific provider.', () => {
+        const mediaPlayer = new MediaPlayer();
+        expect(mediaPlayer.connect()).toEqual('');
+        expect(mediaPlayer.getPlaylist()).toEqual([]);
+
+        mediaPlayer.setProvider(new SpotifyAPI());        
+        expect(mediaPlayer.connect()).toEqual('Connected to Spotify');
+
+        const playlist = mediaPlayer.getPlaylist();
+        expect(playlist).toEqual(['spotify.song1', 'spotify.song2', 'spotify.song3']);
+
+        expect(mediaPlayer.play(playlist[0])).toEqual('Playing spotify.song1');
+        expect(mediaPlayer.play(playlist[1])).toEqual('Playing spotify.song2');
+        expect(mediaPlayer.play(playlist[2])).toEqual('Playing spotify.song3');
+    });
+});
