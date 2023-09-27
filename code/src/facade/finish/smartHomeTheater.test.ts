70f0f4050991d3f1f83c4baa4bdae36f6b19dc06
Olarn U <olarn.u@odds.team>
2023-09-27 13:59:39 +0700

    Finish Facade example code. Update slide for Facade pattern.

--- code/src/facade/finish/smartHomeTheater.test.ts
+++ code/src/facade/finish/smartHomeTheater.test.ts
@@ -1,9 +1,35 @@
+import { SmartHomeTheater } from "./smartHomeTheater";
 import { SmartBulb } from "./smartHomeTheater/smartBulb";
 import { SmartSpeaker } from "./smartHomeTheater/smartSpeaker";
 import { SmartTV } from "./smartHomeTheater/smartTV";
 
 describe('Smart Home Theater', () => {
-    it('should be setup for see the drama movie', () => {
 
+    const expectedOnStatus =
+        'TV: on, input: hdmi1, picture mode: movie\n' +
+        'Speaker: Front Left is on, volume: 10, base: 5, triple: 5\n' +
+        'Speaker: Front Right is on, volume: 10, base: 5, triple: 5\n' +
+        'Speaker: Subwoofer is on, volume: 10, base: 5, triple: 5\n' +
+        'Bulb: Living Room Bulb 1 is on, color: yellow, brightness: 50\n' +
+        'Bulb: Living Room Bulb 2 is on, color: yellow, brightness: 50\n' +
+        'Bulb: Living Room Down Light is on, color: orange, brightness: 50';
+    
+    const expectedOffStatus =
+        'TV: off\n' +
+        'Speaker: Front Left is off\n' +
+        'Speaker: Front Right is off\n' +
+        'Speaker: Subwoofer is off\n' +
+        'Bulb: Living Room Bulb 1 is off\n' +
+        'Bulb: Living Room Bulb 2 is off\n' +
+        'Bulb: Living Room Down Light is off';
+
+    it('should be setup for see the drama movie', () => {
+        const homeTheater = new SmartHomeTheater();
+
+        homeTheater.watchMovie();
+        expect(homeTheater.currentStatus()).toEqual(expectedOnStatus);
+
+        homeTheater.leave();
+        expect(homeTheater.currentStatus()).toEqual(expectedOffStatus);
     });
 });
---
+++ code/src/facade/finish/smartHomeTheater.ts
@@ -0,0 +1,68 @@
+import { SmartBulb } from "./smartHomeTheater/smartBulb";
+import { SmartSpeaker } from "./smartHomeTheater/smartSpeaker";
+import { SmartTV } from "./smartHomeTheater/smartTV";
+
+export class SmartHomeTheater {
+    private tv = new SmartTV();
+
+    private frontLeftSpeaker = new SmartSpeaker('Front Left');
+    private frontRightSpeaker = new SmartSpeaker('Front Right');
+    private subwoofer = new SmartSpeaker('Subwoofer');
+
+    private livingRoomBulb1 = new SmartBulb('Living Room Bulb 1');
+    private livingRoomBulb2 = new SmartBulb('Living Room Bulb 2');
+    private livingRoomDownloght = new SmartBulb('Living Room Down Light');
+
+    watchMovie() {
+        this.tv.turnOn();
+        this.tv.setInput('hdmi1');
+        this.tv.setPictureMode('movie');
+
+        this.frontLeftSpeaker.turnOn();
+        this.frontLeftSpeaker.setVolume(10);
+        this.frontLeftSpeaker.setBase(5);
+        this.frontLeftSpeaker.setTriple(5);
+
+        this.frontRightSpeaker.turnOn();
+        this.frontRightSpeaker.setVolume(10);
+        this.frontRightSpeaker.setBase(5);
+        this.frontRightSpeaker.setTriple(5);
+
+        this.subwoofer.turnOn();
+        this.subwoofer.setVolume(10);
+        this.subwoofer.setBase(5);
+        this.subwoofer.setTriple(5);
+
+        this.livingRoomBulb1.turnOn();
+        this.livingRoomBulb1.setColor('yellow');
+        this.livingRoomBulb1.setBrightness(50);
+
+        this.livingRoomBulb2.turnOn();
+        this.livingRoomBulb2.setColor('yellow');
+        this.livingRoomBulb2.setBrightness(50);
+
+        this.livingRoomDownloght.turnOn();
+        this.livingRoomDownloght.setColor('orange');
+        this.livingRoomDownloght.setBrightness(50);
+    }
+
+    currentStatus(): string {
+        return `${this.tv.currentStatus()}\n` +
+            `${this.frontLeftSpeaker.currentStatus()}\n` +
+            `${this.frontRightSpeaker.currentStatus()}\n` +
+            `${this.subwoofer.currentStatus()}\n` +
+            `${this.livingRoomBulb1.currentStatus()}\n` +
+            `${this.livingRoomBulb2.currentStatus()}\n` +
+            `${this.livingRoomDownloght.currentStatus()}`;
+    }
+
+    leave() {
+        this.tv.turnOff();
+        this.frontLeftSpeaker.turnOff();
+        this.frontRightSpeaker.turnOff();
+        this.subwoofer.turnOff();
+        this.livingRoomBulb1.turnOff();
+        this.livingRoomBulb2.turnOff();
+        this.livingRoomDownloght.turnOff();
+    }
+}
