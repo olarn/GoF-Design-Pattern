9ed609358607fb152a45626a4028348c3f64dc7f
Mawin <kobori1590@gmail.com>
2023-10-16 08:25:04 +0700

    [UPDATE] : update i transform file interface.

--- code/src/template/finish/ITransformData.ts
+++ code/src/template/finish/ITransformData.ts
@@ -1,10 +1,10 @@
 export interface Employee {
-  username: string,
-  number: number,
-  job: string
+    username: string,
+    number: number,
+    job: string
 }
 
 export interface ITransformData {
-  transformDataToObject: (csvData: string[][]) => void
-  readFile: (fileName: string) => string[][]
+    transformDataToObject: (csvData: string[][]) => void
+    readFile: (fileName: string) => { name: string, data: string[][] | number[][] }[]
 }
