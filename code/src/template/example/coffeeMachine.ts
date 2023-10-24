b884933102237843eddc7d6b1e146b81830a490a
Olarn U <olarn.u@odds.team>
2023-10-24 17:08:10 +0700

    Add coffee brew example in the template method patern.

---
+++ code/src/template/example/coffeeMachine.ts
@@ -0,0 +1,32 @@
+export class CoffeeMachine {
+    private waterIsBoilded = false;
+    private CoffeeGrindsIsBrewed = false;
+    private pouredInCup = false;
+    private sugarAndMilkAdded = false;
+
+    public brew(): string {
+        this.boildWater();
+        this.brewCoffeeGrinds();
+        this.pourInCup();
+        this.addSugarAndMilk();
+        
+        return 'Here is your coffee!';
+    }
+
+    private brewCoffeeGrinds() {
+        this.CoffeeGrindsIsBrewed = true;
+
+    }
+
+    private boildWater() {
+        this.waterIsBoilded = true; 
+    }
+
+    private pourInCup() {
+        this.pouredInCup = true;
+    }
+
+    private addSugarAndMilk() {
+        this.sugarAndMilkAdded = true;
+    }
+}
---
+++ code/src/template/example/teaMachine.ts
@@ -0,0 +1,32 @@
+export class TeaMachine {
+    private waterIsBoilded = false;
+    private TeaGrindsIsBrewed = false;
+    private pouredInCup = false;
+    private sugarAdded = false;
+
+    public brew(): string {
+        this.boildWater();
+        this.brewTeaGrinds();
+        this.pourInCup();
+        this.addSugar();
+        
+        return 'Here is your tea!';
+    }
+
+    private brewTeaGrinds() {
+        this.TeaGrindsIsBrewed = true;
+
+    }
+
+    private boildWater() {
+        this.waterIsBoilded = true; 
+    }
+
+    private pourInCup() {
+        this.pouredInCup = true;
+    }
+
+    private addSugar() {
+        this.sugarAdded = true;
+    }
+}
