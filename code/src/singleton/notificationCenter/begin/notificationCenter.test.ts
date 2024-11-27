cdf0d7b484bb483e021eedb98d1fb3ca0e109b32
Olarn U <olarn.u@odds.team>
2024-11-27 09:25:30 +0700

    Add step and test files in Singleton lab.

---
+++ code/src/singleton/notificationCenter/begin/notificationCenter.test.ts
@@ -0,0 +1,12 @@
+import { NotificationCenter } from './notificationCenter';
+import { DiscordObserver } from './observers/discordObserver';
+import { SlackObserver } from './observers/slackObserver';
+import { TelegramObserver } from './observers/telegramObserver';
+
+describe('[Singleton - lab] NotificationCenter', () => {
+  it('should notify on Slack and Discord (only 2), not telegram.', () => {
+    // given
+    // when
+    // then
+  });
+});
